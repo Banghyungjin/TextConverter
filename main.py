bb92f7ea934e4f603e3e0e6e1c0f08293e7598ea
banghyungjin <banghyungjin@gmail.com>
2022-03-28 22:02:37 +0900

    2022_03_28-01 changed method convert_text

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-def convert_text(input_text):
+def convert_text(input_text):  # method that converts string into hex code
     binary_converted = ' '.join(format(c, 'x') for c in bytearray(input_text, "utf-8"))
     print(binary_converted)
 
 
 if __name__ == '__main__':
-    print('Please enter sentence you want to conver into hex code')
+    print('Please enter sentence you want to convert into hex code')
     convert_text(input())
